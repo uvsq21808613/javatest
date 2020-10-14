23e960ac9339d4e0e8c63cbfbd5cf1df001f90cc
uvsq21808613 <sasy.boutata@gmail.com>
2020-10-14 15:05:26 +0200

    new test

--- tod-td2-uvsq21808613-master/src/main/java/fr/uvsq/tod/compte/Compte.java
+++ tod-td2-uvsq21808613-master/src/main/java/fr/uvsq/tod/compte/Compte.java
@@ -18,27 +18,29 @@ public class Compte {
 	  this.solde = initialBalance ;
   }
 	
-	public void    Debit (int  Somme ) throws Exception  { 
+	public boolean    Debit (int  Somme ) throws Exception  { 
 		
 		
 		if(Somme < 0) {
 			throw new Exception("Opération echouée la somme mise est negative ");
 		}
 			
-		this.solde = solde + Somme ;		
+		this.solde = solde + Somme ;	
+		return true ;
 		
 	}
 	
 	
-	public void Credit (int  Money) throws Exception  {
+	public boolean Credit (int  Money) throws Exception  {
 		if ( (Money >solde ) || (Money <0) ) {
 			throw new Exception ( "le solde du compte est insufisant  ");
 			}
 			
 			this.solde =solde -Money ;
+			return true ;
 	}
 	
-	public void versement (int montant ,Compte  Binificaire ,Compte  Crediteur  ) throws Exception   {
+	public boolean versement (int montant ,Compte  Binificaire ,Compte  Crediteur  ) throws Exception   {
 		if (montant > solde ) {
 			throw new Exception ( " verifiez votre solde ");
 			
@@ -46,6 +48,7 @@ public class Compte {
 		Binificaire.solde = Binificaire.solde +montant ;
 		Crediteur.solde = Crediteur.solde -montant ;
 		System.out.println("versement effectuer  ,votre solde est " + this.solde );
+		return true ;
 	}
 	
  
--- tod-td2-uvsq21808613-master/src/test/java/fr/uvsq/tod/compte/CompteTest.java
+++ tod-td2-uvsq21808613-master/src/test/java/fr/uvsq/tod/compte/CompteTest.java
@@ -7,13 +7,27 @@ public class CompteTest {
   
 @Test
   public void shouldCreateAnAccountWithTheSpecifiedBalance() throws Exception {
-    Compte c = new Compte(-100);    
+    Compte c = new Compte(100);    
     assertEquals( 100 , c.getBalance());
+    
   }
 @Test 
   public void shouldCreditMyAccountWithTheSpecifiedBalance() throws Exception {
-	
+    Compte c = new Compte(100);    
+    assertTrue (c.Debit(50));
+  }
+@Test 
+ public void shouldWithdrawMyAccountWithTheSpecifiedBalance()throws Exception {
+	Compte c =new Compte (100);
+	assertTrue (c.Credit (40));
+	}
+@Test 
+	public void shouldSendMoneyWithTheSpecifiedBalance() throws Exception {
+	Compte c =new Compte (100);
+	assertTrue (c.versement(50, c, c));
+	assertEquals( 100 , c.getBalance());
 }
+
 }
 
 	 
